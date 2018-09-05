161adbbf11f0c00f0ada99296fac94c783060e02
Christian Cruz <christiancruz@christians-mac.local>
2018-09-04 21:05:15 -0700

    Initial Commit

---
+++ Entre Pares QR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Entre Pares QR
+//
+//  Created by Christian Cruz on 9/4/18.
+//  Copyright © 2018 Christian Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
